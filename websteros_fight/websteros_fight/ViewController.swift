85b9e707a0794dfe552a8b88341748904f5c8d13
Maximedu13 <maxime.j.pro@free.fr>
2019-03-18 22:14:25 +0100

    list added

--- websteros_fight/websteros_fight/ViewController.swift
+++ websteros_fight/websteros_fight/ViewController.swift
@@ -9,13 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var characters_list = ["Cersei Lannister", "Jaime Lannister", "Tyrion Lannister", "Sansa Stark", "Arya Stark", "Samwell Tarly", "Le Roi de la Nuit", "Ellaria Sand", "Jon Snow"]
 
+ 
     @IBOutlet weak var ui_result: UILabel!
     
     @IBAction func ui_launch() {
     }
     @IBAction func digit_character(_ sender: UIButton) {
         ui_result.text = "\(sender.tag)"
+        for (index, character) in characters_list.enumerated() {
+            if sender.tag == index{
+                ui_result.text = characters_list[index]
+            }
+        }
     }
     
     override func viewDidLoad() {
