df6019a56664ba5ee44da29ace51848839cfdef9
Maximedu13 <maxime.j.pro@free.fr>
2019-03-19 19:59:25 +0100

    main function fixes

--- websteros_fight/websteros_fight/ViewController.swift
+++ websteros_fight/websteros_fight/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     var characters_list = ["Cersei Lannister", "Jaime Lannister", "Tyrion Lannister", "Sansa Stark", "Arya Stark", "Samwell Tarly", "Le Roi de la Nuit", "Ellaria Sand", "Jon Snow"]
-    var characters_chosen = [] as NSArray
+    var character_chosen: [String] = []
     var _currentCharacter : Double = 0 {
         didSet{
             ui_result.text = "\(_currentCharacter)"
@@ -19,22 +19,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var ui_result: UILabel!
     
     @IBAction func ui_launch() {
+        
     }
     
     
  
     @IBAction func digit_character(_ sender: UIButton) {
-         _currentCharacter = _currentCharacter * 10 + Double(sender.tag)
+         //_currentCharacter = _currentCharacter * 10 + Double(sender.tag)
         for (index, character) in characters_list.enumerated() {
             if sender.tag == index{
-                ui_result.text = characters_list[index]
+                ui_result.text? = ""
+                character_chosen.append(characters_list[index])
+                for (item, cha) in character_chosen.enumerated() {
+                    if character_chosen.count == 1{
+                         ui_result.text? = cha
+                    }
+                    if character_chosen.count == 2{
+                        ui_result.text? = character_chosen[0] + " contre " + character_chosen[1]
+                    }
+                    else if character_chosen.count > 2{
+                        reset()
+                    }
+                }
             }
         }
         
     }
-    
-    @IBAction func addCharacter() {
-        
+
+    @IBAction func reset() {
+        character_chosen.removeAll()
+        _currentCharacter = 0
+        ui_result.text? = "Choisissez deux personnages"
     }
     
     override func viewDidLoad() {
